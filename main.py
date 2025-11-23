5669d69a3b1b368f3b2ebc06b9ced97714ad03e4
timesnewronann <ronanborja26@gmail.com>
2025-11-23 13:31:34 -0800

    Added sys args to handle different text inputs and got rid of hardcoded values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_characters, chars_dict_to_sorted_list
+import sys
 
 
 def get_book_text(path_to_file):
@@ -23,7 +24,11 @@ def print_report(book_path, num_words, chars_sorted_list):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 {sys.argv[0]} <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)  # Read file ONCE
     num_words = count_words(text)  # Pass text, get result back
     print(f"Found {num_words} total words")  # Now YOU control when to print
@@ -34,4 +39,5 @@ def main():
     print_report(book_path, num_words, chars_sorted_list)
 
 
-main()
+if __name__ == "__main__":
+    main() 
